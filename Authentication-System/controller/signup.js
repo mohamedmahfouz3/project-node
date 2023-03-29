96723cc66cee38570923348f939da6d889323efa
mohamedsalah112 <ms2000.mohamedsalah@gmail.com>
2023-03-29 21:55:11 +0200

    update signup

--- Authentication-System/controller/signup.js
+++ Authentication-System/controller/signup.js
@@ -1,4 +1,16 @@
-
+const express = require('express');
+const crypto = require('crypto');
+require('express-async-errors');
+const { body } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const dotenv = require('dotenv');
+const BadRequestError = require('../errors/bad-request-error');
+const validateRequest = require('../middlewares/validate-request');
+const currentUser = require('../middlewares/current-user');
+const { error } = require('console');
+const router = express.Router();
+dotenv.config();
 
 
 const signup = async (req, res) => {
@@ -6,6 +18,20 @@ const signup = async (req, res) => {
 
   const existingUser = await User.findOne({ email });
 
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
+  }
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  await user.save();
+
+
+
+  res.status(201).send(user);
 }
 
   module.exports = {signup}

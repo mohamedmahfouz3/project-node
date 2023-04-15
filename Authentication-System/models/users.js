447d7138da93b627806c377084f6bbc354cee1f4
Ramzi Muhamma'd <ramzi180332@feng.bu.edu.eg>
2023-04-15 18:56:55 +0200

    Edit our schema

--- Authentication-System/models/users.js
+++ Authentication-System/models/users.js
@@ -2,80 +2,26 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
-const schema = mongoose.Schema;
-const UserSchema = new schema(
-  {
-    name: {
-      type: String,
-      lowercase: true,
-      required: [true, 'name require'],
-    },
 
-    email: {
-      type: String,
-      required: [true, 'email required'],
-      unique: true,
-      lowercase: true,
-    },
+const all_users_Schema = mongoose.Schema({
+    name: { type :String, required: true },
+    email: { type : String, required: true },
+    password: { type :String, required: true },
+ 
+  });
+   
+ 
+  all_users_Schema.pre('save', async function (next) {
+    try {
+      const salt = await bcrypt.genSalt(12);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  
 
-    password: {
-      type: String,
-      required: [true, 'password required'],
-      minLength: [8, 'too short password'],
-    },
-
-    passwordConfirm: {
-      type: String,
-      required: [false, 'password required'],
-      minLength: [8, 'too short password'],
-    },
-    passwordChangedAt: {
-      type: Date,
-      required: false,
-    },
-    passwordResetUserJwt: { type: String },
-
-    resetPasswordExpires: {
-      type: Date,
-      required: false,
-    },
-
-    role: {
-      type: String,
-      enum: [
-        'instructor',
-        'admin',
-        'coordinator',
-        'Head of Department',
-        'dean',
-      ],
-      default: 'instructor',
-    },
-  },
-  { timestamps: true }
-);
-
-UserSchema.pre('save', async function (next) {
-  try {
-    const salt = await bcrypt.genSalt(12);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-UserSchema.methods.createPasswordResetToken = function () {
-  const resetUserJwt = crypto.randomBytes(32).toString('hex');
-  this.passwordResetUserJwt = crypto
-    .createHash('sha256')
-    .update(resetUserJwt)
-    .digest('hex');
-  console.log({ resetUserJwt }, this.passwordResetUserJwt);
-  return resetUserJwt;
-};
-
-const User = mongoose.model('User', UserSchema);
-
-module.exports = User;
+module.exports = all_users_Schema;
